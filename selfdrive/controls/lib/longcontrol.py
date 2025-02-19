27d768f0737d2d7d799333d5cd3c65def2418883
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2025-02-19 17:31:02 +0800

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -86,7 +86,7 @@ def long_control_state_trans_old_long(CP, active, long_control_state, v_ego, v_t
   return long_control_state
 
 
-cclass LongControl:
+class LongControl:
   def __init__(self, CP):
     self.CP = CP
     self.long_control_state = LongCtrlState.off
