mouxangithub/openpilot

FrogPilot 0.9.7

tinygrad_repo/extra/assembly/assembly_arm64.py
@@ -1,9 +1,10 @@
 import struct
 from platform import system
 from typing import Tuple, Dict, List, Optional
-from tinygrad.ops import BinaryOps, UnaryOps, TernaryOps
-from tinygrad.codegen.linearizer import UOps, UOp
-from tinygrad.helpers import dtypes, CI
+from tinygrad import dtypes
+from tinygrad.uop.ops import BinaryOps, UnaryOps, TernaryOps
+from tinygrad.codegen.kernel import Ops, UOp
+from tinygrad.helpers import CI
 from tinygrad.codegen.assembly import uops_to_asmstyle, AssemblyLanguage
 
 def float_to_hex(x): return "%02X%02X%02X%02X" % tuple(struct.pack("f",x)[::-1])
@@ -18,7 +19,7 @@ class ARM64Language(AssemblyLanguage): pass
 
 def specialize_to_arm64(fn_nm, asm):
   var_size = 16
-  prev_uop:Optional[UOps] = None
+  prev_uop:Optional[Ops] = None
   ins = []
   x_regs = ['x' + str(i) for i in reversed(range(12))]
   s_regs = ['s' + str(i) for i in reversed(range(3,32)) if i <= 7 or i >= 16]
@@ -80,7 +81,7 @@ def specialize_to_arm64(fn_nm, asm):
       ins.append(f"mov x15, {mem_vars[v.nm]}")
       ins.append(f"ldr {rtor[v.nm]}, [sp, x15]")
 
-    if uop == UOps.SPECIAL:
+    if uop == Ops.SPECIAL:
       if arg.startswith('data'):
         # data 8 to n into the stack
         if int(arg[4:]) >= 8:
@@ -89,7 +90,7 @@ def specialize_to_arm64(fn_nm, asm):
       else:
         ins.append(f"mov {rtor[out.nm]}, #0")
         ins.append(f"loop_{arg}:")
-    elif uop == UOps.CAST:
+    elif uop == Ops.CAST:
       if arg == BinaryOps.CMPLT:
         if rtor[out.nm][0] == 's':
           mov_imm(0.0, 's0')
@@ -101,7 +102,7 @@ def specialize_to_arm64(fn_nm, asm):
           ins.append(f"csel {rtor[out.nm]}, x15, x14, lt")
       else:
         ins.append(f"sxtw {rtor[out.nm]}, w{rtor[vin[0].nm][1:]}")
-    elif uop == UOps.ALU:
+    elif uop == Ops.ALU:
       if len(vin)==2 and vin[1].__class__ is int: mov_imm(vin[1], 'x15')
       if arg == BinaryOps.MUL and out.dtype == dtypes.bool:
         ins.append(f"ands {','.join('x15' if v.__class__ is int else rtor[v.nm] for v in [out] + vin)}")
@@ -135,7 +136,7 @@ def specialize_to_arm64(fn_nm, asm):
         ins.append(f"msub {rtor[out.nm]}, x14, {rhs}, {rtor[vin[0].nm]}")
       else:
         ins.append(f"{'f' if dtypes.is_float(vin[0][1]) else 's' if arg == BinaryOps.DIV else ''}{alu[arg]} {', '.join('x15' if v.__class__ is int else rtor[v.nm] for v in [out] + vin)}")
-    elif uop == UOps.LOAD:
+    elif uop == Ops.LOAD:
       if arg.__class__ in (int, float):
         mov_imm(arg, rtor[out.nm])
       else:
@@ -145,20 +146,20 @@ def specialize_to_arm64(fn_nm, asm):
         ins.append(f"add x15, {rtor[vin[0].nm]}, x15")
         ins.append(f"ldr{'sb' if arg[2] is not None and arg[2] in (dtypes.int8, dtypes.uint8, dtypes.bool) else ''} {reg_in}, [x15]")
         if arg[2] is not None: ins.append(f"{'fcvt' if arg[2] in [dtypes.half, dtypes.double] else 'scvtf'} {rtor[out.nm]}, {reg_in}")
-    elif uop == UOps.STORE:
+    elif uop == Ops.STORE:
       #NOTE: if need casting load var in s/h0 or x/w12 temp regs
       reg_out = (type_to_reg[arg[2]] + ('0' if dtypes.is_float(arg[2]) else '12') if arg[2] is not None else rtor[vin[1].nm])
       if arg[2] is not None: ins.append(f"fcvt{'zs' if arg[2] not in [dtypes.half, dtypes.double] else '' } {reg_out}, {rtor[vin[1].nm]}")
       ins.append(f"mov x15, #{arg[0]}")
       ins.append(f"str {reg_out}, [{rtor[vin[0].nm]}, x15, lsl #0]")
-    elif uop == UOps.COND_BRANCH:
+    elif uop == Ops.COND_BRANCH:
       #TODO: this is a hack it shouldn't always be a cmp before a cond branch?
-      if prev_uop == UOps.LOAD:
+      if prev_uop == Ops.LOAD:
         ins.append(f"cmp {rtor[vin[0].nm]}, #0")
       ins.append(f"b.{'lt' if arg[1] else 'ge'} {arg[0][1:]}")
-    elif uop == UOps.LABEL:
+    elif uop == Ops.LABEL:
       ins.append(f"{arg[1:]}:")
-    elif uop == UOps.ENDLOOP:
+    elif uop == Ops.ENDLOOP:
       mov_imm(arg[0], "x15")
       ins.append(f"add {rtor[vin[0].nm]}, {rtor[vin[0].nm]}, #1")
       ins.append(f"cmp {rtor[vin[0].nm]}, x15")

tinygrad_repo/extra/gemm/tvm_gemm.py
@@ -32,16 +32,15 @@ import os
 from tinygrad.tensor import Tensor
 
 # define the compute
-A = Tensor.rand(M, K, device="clang")
-B = Tensor.rand(K, N, device="clang")
+A = Tensor.rand(M, K, device="CPU")
+B = Tensor.rand(K, N, device="CPU")
 C = (A.reshape(M, 1, K) * B.permute(1,0).reshape(1, N, K)).sum(axis=2)
 
-sched = C.lazydata.schedule()
-from tinygrad.codegen.linearizer import Linearizer
-from tinygrad.codegen.kernel import LinearizerOptions
-lin = Linearizer(sched[-1].ast, LinearizerOptions(has_local=False, supports_float4=False))
-#lin.hand_coded_optimizations()
+sched = C.schedule()
+from tinygrad.codegen.kernel import Kernel
+from tinygrad.device import CompilerOptions
+lin = Kernel(sched[-1].ast, CompilerOptions(has_local=False, supports_float4=False))
 lin.linearize()
-from tinygrad.runtime.ops_clang import renderer
+from tinygrad.runtime.ops_cpu import renderer
 src = renderer("mmult", lin.uops)
 print(src)

tinygrad_repo/tinygrad/engine/memory.py
@@ -1,0 +1,69 @@
+from typing import cast
+from collections import defaultdict
+from tinygrad.engine.schedule import ScheduleItem
+from tinygrad.device import Device, Buffer
+from tinygrad.helpers import NO_MEMORY_PLANNER, dedup, DEBUG, round_up
+from tinygrad.uop.ops import Ops
+from tinygrad.dtype import dtypes, ImageDType
+from tinygrad.runtime.support.allocator import TLSFAllocator
+
+# **************** memory planning ****************
+
+def _internal_memory_planner(buffers:list[list[Buffer]], noopt_buffers=None, ignore_checks=False, debug_prefix="") -> dict[Buffer, Buffer]:
+  if NO_MEMORY_PLANNER: return {}
+  first_appearance, last_appearance, buf_to_opt = {}, {}, set()
+  for i,u in enumerate(buffers):
+    for buf in u:
+      should_skip = buf.is_allocated() or buf.base.is_allocated() or buf.lb_refcount > 0 or (noopt_buffers is not None and buf.base in noopt_buffers)
+      if not ignore_checks and should_skip: continue
+      if buf.base not in first_appearance: first_appearance[buf.base] = i
+      last_appearance[buf.base] = i
+      buf_to_opt.add(buf)
+
+  # Sort buffer operations in timeline order. Two events: buffer is allocated or buffer is freed.
+  buffer_requests = sorted([((first_appearance[buf], True), buf) for buf in first_appearance.keys()] + \
+                           [((last_appearance[buf] + 1, False), buf) for buf in first_appearance.keys()], key=lambda x: x[0])
+
+  # Try to suballocate from a shared buffer managed by global_planner using TLSFAllocator.
+  # Also track buffer replacements for buffers that do not support suballocation.
+  buffer_replace:dict[Buffer, tuple[Buffer|None, int|None]] = {}
+  reuse_buffers:dict[tuple, list[Buffer]] = defaultdict(list)
+  global_planner:dict[str, tuple[int, TLSFAllocator]] = defaultdict(lambda: (0, TLSFAllocator(1 << 44, block_size=0x1000, lv2_cnt=32)))
+  for (_, is_open_ev), buf in buffer_requests:
+    # Check if suballocation is possible for the given buffer and device.
+    if hasattr(Device[buf.device].allocator, "_offset") and not isinstance(buf.dtype, ImageDType):
+      if is_open_ev: buffer_replace[buf] = (None, global_planner[buf.device][1].alloc(round_up(buf.nbytes, 0x1000)))
+      else: global_planner[buf.device][1].free(cast(int, buffer_replace[buf][1]))
+      global_planner[buf.device] = (max(global_planner[buf.device][0], buffer_replace[buf][1] + buf.nbytes), global_planner[buf.device][1])
+    else:
+      key = (buf.device, buf.dtype, buf.options, buf.nbytes)
+      if is_open_ev: buffer_replace[buf] = (reuse_buffers[key].pop(), None) if key in reuse_buffers and len(reuse_buffers[key]) > 0 else (buf, None)
+      else: reuse_buffers[key].append(cast(Buffer, buffer_replace[buf][0]))
+
+  # Allocate global buffers based on the memory planner.
+  global_buffers = {dev: Buffer(dev, round_up(sz, 0x1000), dtypes.int8) for dev, (sz, _) in global_planner.items()}
+  buffer_resolve:dict[Buffer, tuple[Buffer, int|None]] = {buf: (base or global_buffers[buf.device], off) for buf,(base,off) in buffer_replace.items()}
+
+  # Assign buffers. First, assign full buffers (not sub-buffers).
+  assigned:dict[Buffer, Buffer] = {}
+  for buf, (base, off) in buffer_resolve.items():
+    if buf != base:
+      assigned[buf] = base if off is None else Buffer(buf.device, buf.size, buf.dtype, base=base, offset=off)
+
+  # Now assign sub-buffers.
+  for buf in buf_to_opt:
+    if buf._base is not None:
+      assigned[buf] = Buffer(buf.device, buf.size, buf.dtype, base=(pbuf:=assigned.get(buf.base, buf.base)).base, offset=pbuf.offset+buf.offset)
+
+  if DEBUG >= 1:
+    ak, av = dedup(x for x in assigned.keys() if x._base is None),dedup(x for x in assigned.values() if x._base is None)+list(global_buffers.values())
+    omem, nmem = sum([x.nbytes for x in ak])/1e6, sum([x.nbytes for x in av])/1e6
+    if omem != nmem: print(f"{debug_prefix}memory reduced from {omem:.2f} MB -> {nmem:.2f} MB,", f"{len(ak)} -> {len(av)} bufs")
+
+  return assigned
+
+def memory_planner(schedule:list[ScheduleItem]) -> list[ScheduleItem]:
+  # Exclude buffers involved in load ops (e.g transfers) to preserve parallelism in graphs.
+  assigned = _internal_memory_planner([list(si.bufs) for si in schedule],
+                                      noopt_buffers={b for si in schedule if si.ast.op is not Ops.SINK for b in si.bufs})
+  return [ScheduleItem(si.ast, tuple(assigned.get(x, x) for x in si.bufs), si.metadata, si.fixedvars) for si in schedule]

dsa302010/dp

tinygrad_repo/extra/assembly/assembly_arm64.py
@@ -1,9 +1,10 @@
 import struct
 from platform import system
 from typing import Tuple, Dict, List, Optional
-from tinygrad.ops import BinaryOps, UnaryOps, TernaryOps
-from tinygrad.codegen.linearizer import UOps, UOp
-from tinygrad.helpers import dtypes, CI
+from tinygrad import dtypes
+from tinygrad.uop.ops import BinaryOps, UnaryOps, TernaryOps
+from tinygrad.opt.kernel import Ops, UOp
+from tinygrad.helpers import CI
 from tinygrad.codegen.assembly import uops_to_asmstyle, AssemblyLanguage
 
 def float_to_hex(x): return "%02X%02X%02X%02X" % tuple(struct.pack("f",x)[::-1])
@@ -18,7 +19,7 @@ class ARM64Language(AssemblyLanguage): pass
 
 def specialize_to_arm64(fn_nm, asm):
   var_size = 16
-  prev_uop:Optional[UOps] = None
+  prev_uop:Optional[Ops] = None
   ins = []
   x_regs = ['x' + str(i) for i in reversed(range(12))]
   s_regs = ['s' + str(i) for i in reversed(range(3,32)) if i <= 7 or i >= 16]
@@ -80,7 +81,7 @@ def specialize_to_arm64(fn_nm, asm):
       ins.append(f"mov x15, {mem_vars[v.nm]}")
       ins.append(f"ldr {rtor[v.nm]}, [sp, x15]")
 
-    if uop == UOps.SPECIAL:
+    if uop == Ops.SPECIAL:
       if arg.startswith('data'):
         # data 8 to n into the stack
         if int(arg[4:]) >= 8:
@@ -89,7 +90,7 @@ def specialize_to_arm64(fn_nm, asm):
       else:
         ins.append(f"mov {rtor[out.nm]}, #0")
         ins.append(f"loop_{arg}:")
-    elif uop == UOps.CAST:
+    elif uop == Ops.CAST:
       if arg == BinaryOps.CMPLT:
         if rtor[out.nm][0] == 's':
           mov_imm(0.0, 's0')
@@ -101,7 +102,7 @@ def specialize_to_arm64(fn_nm, asm):
           ins.append(f"csel {rtor[out.nm]}, x15, x14, lt")
       else:
         ins.append(f"sxtw {rtor[out.nm]}, w{rtor[vin[0].nm][1:]}")
-    elif uop == UOps.ALU:
+    elif uop == Ops.ALU:
       if len(vin)==2 and vin[1].__class__ is int: mov_imm(vin[1], 'x15')
       if arg == BinaryOps.MUL and out.dtype == dtypes.bool:
         ins.append(f"ands {','.join('x15' if v.__class__ is int else rtor[v.nm] for v in [out] + vin)}")
@@ -135,7 +136,7 @@ def specialize_to_arm64(fn_nm, asm):
         ins.append(f"msub {rtor[out.nm]}, x14, {rhs}, {rtor[vin[0].nm]}")
       else:
         ins.append(f"{'f' if dtypes.is_float(vin[0][1]) else 's' if arg == BinaryOps.DIV else ''}{alu[arg]} {', '.join('x15' if v.__class__ is int else rtor[v.nm] for v in [out] + vin)}")
-    elif uop == UOps.LOAD:
+    elif uop == Ops.LOAD:
       if arg.__class__ in (int, float):
         mov_imm(arg, rtor[out.nm])
       else:
@@ -145,20 +146,20 @@ def specialize_to_arm64(fn_nm, asm):
         ins.append(f"add x15, {rtor[vin[0].nm]}, x15")
         ins.append(f"ldr{'sb' if arg[2] is not None and arg[2] in (dtypes.int8, dtypes.uint8, dtypes.bool) else ''} {reg_in}, [x15]")
         if arg[2] is not None: ins.append(f"{'fcvt' if arg[2] in [dtypes.half, dtypes.double] else 'scvtf'} {rtor[out.nm]}, {reg_in}")
-    elif uop == UOps.STORE:
+    elif uop == Ops.STORE:
       #NOTE: if need casting load var in s/h0 or x/w12 temp regs
       reg_out = (type_to_reg[arg[2]] + ('0' if dtypes.is_float(arg[2]) else '12') if arg[2] is not None else rtor[vin[1].nm])
       if arg[2] is not None: ins.append(f"fcvt{'zs' if arg[2] not in [dtypes.half, dtypes.double] else '' } {reg_out}, {rtor[vin[1].nm]}")
       ins.append(f"mov x15, #{arg[0]}")
       ins.append(f"str {reg_out}, [{rtor[vin[0].nm]}, x15, lsl #0]")
-    elif uop == UOps.COND_BRANCH:
+    elif uop == Ops.COND_BRANCH:
       #TODO: this is a hack it shouldn't always be a cmp before a cond branch?
-      if prev_uop == UOps.LOAD:
+      if prev_uop == Ops.LOAD:
         ins.append(f"cmp {rtor[vin[0].nm]}, #0")
       ins.append(f"b.{'lt' if arg[1] else 'ge'} {arg[0][1:]}")
-    elif uop == UOps.LABEL:
+    elif uop == Ops.LABEL:
       ins.append(f"{arg[1:]}:")
-    elif uop == UOps.ENDLOOP:
+    elif uop == Ops.ENDLOOP:
       mov_imm(arg[0], "x15")
       ins.append(f"add {rtor[vin[0].nm]}, {rtor[vin[0].nm]}, #1")
       ins.append(f"cmp {rtor[vin[0].nm]}, x15")

tinygrad_repo/extra/gemm/tvm_gemm.py
@@ -32,16 +32,15 @@ import os
 from tinygrad.tensor import Tensor
 
 # define the compute
-A = Tensor.rand(M, K, device="clang")
-B = Tensor.rand(K, N, device="clang")
+A = Tensor.rand(M, K, device="CPU")
+B = Tensor.rand(K, N, device="CPU")
 C = (A.reshape(M, 1, K) * B.permute(1,0).reshape(1, N, K)).sum(axis=2)
 
-sched = C.lazydata.schedule()
-from tinygrad.codegen.linearizer import Linearizer
-from tinygrad.codegen.kernel import LinearizerOptions
-lin = Linearizer(sched[-1].ast, LinearizerOptions(has_local=False, supports_float4=False))
-#lin.hand_coded_optimizations()
-lin.linearize()
-from tinygrad.runtime.ops_clang import renderer
+sched = C.schedule()
+from tinygrad.opt.kernel import Kernel
+from tinygrad.device import CompilerOptions
+lin = Kernel(sched[-1].ast, CompilerOptions(has_local=False, supports_float4=False))
+lin.to_program()
+from tinygrad.runtime.ops_cpu import renderer
 src = renderer("mmult", lin.uops)
 print(src)

tinygrad_repo/tinygrad/engine/memory.py
@@ -1,0 +1,69 @@
+from typing import cast
+from collections import defaultdict
+from tinygrad.engine.schedule import ScheduleItem
+from tinygrad.device import Device, Buffer
+from tinygrad.helpers import NO_MEMORY_PLANNER, dedup, DEBUG, round_up
+from tinygrad.uop.ops import Ops
+from tinygrad.dtype import dtypes, ImageDType
+from tinygrad.runtime.support.memory import TLSFAllocator
+
+# **************** memory planning ****************
+
+def _internal_memory_planner(buffers:list[list[Buffer]], noopt_buffers=None, ignore_checks=False, debug_prefix="") -> dict[Buffer, Buffer]:
+  if NO_MEMORY_PLANNER: return {}
+  first_appearance, last_appearance, buf_to_opt = {}, {}, set()
+  for i,u in enumerate(buffers):
+    for buf in u:
+      should_skip = buf.is_allocated() or buf.base.is_allocated() or buf.uop_refcount > 0 or (noopt_buffers is not None and buf.base in noopt_buffers)
+      if not ignore_checks and should_skip: continue
+      if buf.base not in first_appearance: first_appearance[buf.base] = i
+      last_appearance[buf.base] = i
+      buf_to_opt.add(buf)
+
+  # Sort buffer operations in timeline order. Two events: buffer is allocated or buffer is freed.
+  buffer_requests = sorted([((first_appearance[buf], True), buf) for buf in first_appearance.keys()] + \
+                           [((last_appearance[buf] + 1, False), buf) for buf in first_appearance.keys()], key=lambda x: x[0])
+
+  # Try to suballocate from a shared buffer managed by global_planner using TLSFAllocator.
+  # Also track buffer replacements for buffers that do not support suballocation.
+  buffer_replace:dict[Buffer, tuple[Buffer|None, int|None]] = {}
+  reuse_buffers:dict[tuple, list[Buffer]] = defaultdict(list)
+  global_planner:dict[str, tuple[int, TLSFAllocator]] = defaultdict(lambda: (0, TLSFAllocator(1 << 44, block_size=0x1000, lv2_cnt=32)))
+  for (_, is_open_ev), buf in buffer_requests:
+    # Check if suballocation is possible for the given buffer and device.
+    if hasattr(Device[buf.device].allocator, "_offset") and not isinstance(buf.dtype, ImageDType):
+      if is_open_ev: buffer_replace[buf] = (None, global_planner[buf.device][1].alloc(round_up(buf.nbytes, 0x1000)))
+      else: global_planner[buf.device][1].free(cast(int, buffer_replace[buf][1]))
+      global_planner[buf.device] = (max(global_planner[buf.device][0], buffer_replace[buf][1] + buf.nbytes), global_planner[buf.device][1])
+    else:
+      key = (buf.device, buf.dtype, buf.options, buf.nbytes)
+      if is_open_ev: buffer_replace[buf] = (reuse_buffers[key].pop(), None) if key in reuse_buffers and len(reuse_buffers[key]) > 0 else (buf, None)
+      else: reuse_buffers[key].append(cast(Buffer, buffer_replace[buf][0]))
+
+  # Allocate global buffers based on the memory planner.
+  global_buffers = {dev: Buffer(dev, round_up(sz, 0x1000), dtypes.int8) for dev, (sz, _) in global_planner.items()}
+  buffer_resolve:dict[Buffer, tuple[Buffer, int|None]] = {buf: (base or global_buffers[buf.device], off) for buf,(base,off) in buffer_replace.items()}
+
+  # Assign buffers. First, assign full buffers (not sub-buffers).
+  assigned:dict[Buffer, Buffer] = {}
+  for buf, (base, off) in buffer_resolve.items():
+    if buf != base:
+      assigned[buf] = base if off is None else Buffer(buf.device, buf.size, buf.dtype, base=base, offset=off)
+
+  # Now assign sub-buffers.
+  for buf in buf_to_opt:
+    if buf._base is not None:
+      assigned[buf] = Buffer(buf.device, buf.size, buf.dtype, base=(pbuf:=assigned.get(buf.base, buf.base)).base, offset=pbuf.offset+buf.offset)
+
+  if DEBUG >= 1:
+    ak, av = dedup(x for x in assigned.keys() if x._base is None),dedup(x for x in assigned.values() if x._base is None)+list(global_buffers.values())
+    omem, nmem = sum([x.nbytes for x in ak])/1e6, sum([x.nbytes for x in av])/1e6
+    if omem != nmem: print(f"{debug_prefix}memory reduced from {omem:.2f} MB -> {nmem:.2f} MB,", f"{len(ak)} -> {len(av)} bufs")
+
+  return assigned
+
+def memory_planner(schedule:list[ScheduleItem]) -> list[ScheduleItem]:
+  # Exclude buffers involved in load ops (e.g transfers) to preserve parallelism in graphs.
+  assigned = _internal_memory_planner([list(si.bufs) for si in schedule],
+                                      noopt_buffers={b for si in schedule if si.ast.op is not Ops.SINK for b in si.bufs})
+  return [ScheduleItem(si.ast, tuple(assigned.get(x, x) for x in si.bufs), si.metadata, si.fixedvars) for si in schedule]